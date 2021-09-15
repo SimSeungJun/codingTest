f12a26277c08dd0cb750f59c93a9aa3aed302b8e
SimSeungJun <ctamegp0@gmail.com>
2021-09-15 21:02:39 +0900

    a+b

---
+++ baekjoon/inOut/n_1000/Main.java
@@ -0,0 +1,15 @@
+package baekjoon.inOut.n_1000;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int A = scanner.nextInt();
+        int B = scanner.nextInt();
+        int sum = A+B;
+        System.out.print(sum);
+
+
+    }
+}
